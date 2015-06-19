a506f3addcd3629a326b28540f6edb4075d64ac9
Paweł Dudziński <pawel.d@webinterpret.com>
2015-06-19 12:41:26 +0200

    modify fab file

--- kj/fabfile.py
+++ kj/fabfile.py
@@ -1,6 +1,16 @@
 from fabric.api import local
 
-def push_changes(message = 'pushed via fabric'):
-    local("git add .")
-    local("git commit -m '%s'" % (message))
-    local("git push origin master")
+class DeployPreparator(object):
+
+    def __init__(self, commit_message):
+        self.commit_message = commit_message
+    
+    def push_changes(self):
+        local("git add .")
+        local("git commit -m '%s'" % (self.commit_message))
+        local("git push origin master")
+        
+        
+def deploy(commit_message='pushed via fabric'):
+    preparator = DeployPreparator(commit_message)
+    preparator.push_changes()
