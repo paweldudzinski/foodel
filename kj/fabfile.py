f572f8bc2964ca2431a50bafd632b567c1ed04a0
Paweł Dudziński <pawel.d@webinterpret.com>
2015-06-26 09:58:44 +0200

    modify fab file

--- kj/fabfile.py
+++ kj/fabfile.py
@@ -1,6 +1,5 @@
 from fabric.api import local, cd, env, run
 
-
 env.hosts = ['web1.mydevil.net']
 env.user = 'papaduda'
 env.password = 'xWmnCtoV'
@@ -9,7 +8,6 @@ class DeployPreparator(object):
 
     def __init__(self, commit_message):
         self.commit_message = commit_message
-        #ConfigProvider.set_environment()
     
     def push_changes(self):
         local("git add .")
